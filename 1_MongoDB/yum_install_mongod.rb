fb5939d40ae3e693466986bd2e43173d802b3f3d
jprooneynj <jprooneynj@gmail.com>
2017-11-15 21:07:36 +0000

    new status

---
+++ 1_MongoDB/yum_install_mongod.rb
@@ -0,0 +1,15 @@
+yum_repository 'mongodb' do
+  description "MongoDB Repository"
+  baseurl "https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.4/x86_64/"
+  gpgkey 'https://www.mongodb.org/static/pgp/server-3.4.asc'
+  action :create
+end
+
+
+yum_package 'mongodb-org'
+
+
+service 'mongod' do
+  action [:enable, :start]
+end
+
