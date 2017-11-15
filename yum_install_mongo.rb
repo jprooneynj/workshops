9ed1e01501f93ea3197ef68963f4a646cebb5936
jprooneynj <33639184+jprooneynj@users.noreply.github.com>
2017-11-14 20:01:38 -0500

    Add files via upload

--- yum_install_mongo.rb
+++ yum_install_mongo.rb
@@ -1,1 +1,7 @@
-yum_package 'mongodb-org-3.4'
+yum_repository 'mongodb' do
+  description "MongoDB Repository"
+  baseurl "https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.4/x86_64/"
+  gpgkey 'https://www.mongodb.org/static/pgp/server-3.4.asc'
+  action :create
+end
+
