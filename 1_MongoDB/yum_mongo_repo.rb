d81a41d9f07c43e6d6ba65d47215d639fa5596df
jprooneynj <33639184+jprooneynj@users.noreply.github.com>
2017-11-14 19:18:07 -0500

    Add files via upload

---
+++ 1_MongoDB/yum_mongo_repo.rb
@@ -0,0 +1,8 @@
+file '/etc/yum.repos.d/mongodb-org-3.4.repo' do
+  content '[mongodb-org-3.4]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.4/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-3.4.asc'
+end
