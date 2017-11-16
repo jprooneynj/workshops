571a80eac5383a49757a77825fe4efce61796bc9
jprooneynj <jprooneynj@gmail.com>
2017-11-16 02:03:33 +0000

    linux commands working

--- 2_Tomcat/cookbooks/Tomcat/recipes/default.rb
+++ 2_Tomcat/cookbooks/Tomcat/recipes/default.rb
@@ -1,4 +1,5 @@
 
+
 #
 # Cookbook:: Tomcat
 # Recipe:: default
@@ -11,8 +12,16 @@
 
 execute 'groupadd' do
   command '/usr/sbin/groupadd tomcat'
+  ignore_failure true
 end
 
 execute 'useradd' do
   command '/usr/sbin/useradd -M -s /bin/nologin -g tomcat -d /opt/tomcat tomcat'
+  ignore_failure true 
+end
+
+
+execute 'get tomcat' do
+  command "wget http://apache.cs.utah.edu/tomcat/tomcat-8/v8.0.47/bin/apache-tomcat-8.0.47.tar.gz"
+  cwd "/tmp"
 end
