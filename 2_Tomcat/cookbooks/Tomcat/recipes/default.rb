732d8428979194d02099058b6f8291223b4b1dcb
jprooneynj <jprooneynj@gmail.com>
2017-11-16 02:33:22 +0000

    added more linux commands

--- 2_Tomcat/cookbooks/Tomcat/recipes/default.rb
+++ 2_Tomcat/cookbooks/Tomcat/recipes/default.rb
@@ -25,3 +25,29 @@ execute 'get tomcat' do
   command "wget http://apache.cs.utah.edu/tomcat/tomcat-8/v8.0.47/bin/apache-tomcat-8.0.47.tar.gz"
   cwd "/tmp"
 end
+
+execute 'mkdir' do
+  command 'mkdir /opt/tomcat'
+  creates '/opt/tomcat'
+  action :run  
+end
+
+execute 'tar' do
+  command "tar xvf apache-tomcat-8*tar.gz.5 -C /opt/tomcat --strip-components=1"
+  cwd "/tmp"
+end
+
+execute 'chmod' do
+  command '/bin/chmod -R g+r conf'
+  ignore_failure true
+end
+
+execute 'chmod1' do
+  command '/bin/chmod g+x conf'
+  ignore_failure true
+end
+
+execute 'chown' do
+  command '/bin/chown -R tomcat webapps/ work/ temp/ logs/'
+  ignore_failure true
+end
