4e1343059dbe7a0a81a54a77041faa106e71019d
jprooneynj <jprooneynj@gmail.com>
2017-11-16 01:39:34 +0000

    linux commands working

--- 2_Tomcat/cookbooks/Tomcat/recipes/default.rb
+++ 2_Tomcat/cookbooks/Tomcat/recipes/default.rb
@@ -1,5 +1,18 @@
+
 #
 # Cookbook:: Tomcat
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+
+# yum_package 'java-1.7.0-openjdk-devel'
+
+
+execute 'groupadd' do
+  command '/usr/sbin/groupadd tomcat'
+end
+
+execute 'useradd' do
+  command '/usr/sbin/useradd -M -s /bin/nologin -g tomcat -d /opt/tomcat tomcat'
+end
